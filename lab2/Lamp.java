9ae9a5b2dca15728c2f8f55d0fa1821d7f6f585f
Natasha S Love <debora@inbox.ru>
2018-09-05 19:24:41 -0500

    get and set

--- lab2/Lamp.java
+++ lab2/Lamp.java
@@ -7,6 +7,21 @@ public class Lamp {
 	
 	//obj methods
 	public void turnOn() {
-		
+		isOn=true;
+	}
+	public void turnOff() {
+		isOn=false;
+	}
+	public void setColor(String c) {
+		color=c;
+	}
+	public String getColor() {
+	return color;
+	}
+	public void setWatts(String w) {
+		watts=w;
+	}
+	public String getWatts() {
+	return watts;
 	}
 }
